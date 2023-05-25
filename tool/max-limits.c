9b141ada3d21eb3b871e18d518e58696e08c7458
Raphael Traviss <raphael@skyleafdesign.com>
2023-05-25 13:38:02 -0500

    Add function to print SQLite limits.

--- tool/max-limits.c
+++ tool/max-limits.c
@@ -1,41 +1,49 @@
-/*
-** Link this program against an SQLite library of unknown provenance in order
-** to display the compile-time maximum values for various settings.
-*/
 #include "sqlite3.h"
 #include <stdio.h>
+#include <stdbool.h>
 
-static const struct {
-  int eCode;
-  char *zName;
-} aLimit[] = {
-  { SQLITE_LIMIT_LENGTH,                "SQLITE_MAX_LENGTH"               },
-  { SQLITE_LIMIT_SQL_LENGTH,            "SQLITE_MAX_SQL_LENGTH"           },
-  { SQLITE_LIMIT_COLUMN,                "SQLITE_MAX_COLUMN"               },
-  { SQLITE_LIMIT_EXPR_DEPTH,            "SQLITE_MAX_EXPR_DEPTH"           },
-  { SQLITE_LIMIT_COMPOUND_SELECT,       "SQLITE_MAX_COMPOUND_SELECT"      },
-  { SQLITE_LIMIT_VDBE_OP,               "SQLITE_MAX_VDBE_OP"              },
-  { SQLITE_LIMIT_FUNCTION_ARG,          "SQLITE_MAX_FUNCTION_ARG"         },
-  { SQLITE_LIMIT_ATTACHED,              "SQLITE_MAX_ATTACHED"             },
-  { SQLITE_LIMIT_LIKE_PATTERN_LENGTH,   "SQLITE_MAX_LIKE_PATTERN_LENGTH"  },
-  { SQLITE_LIMIT_VARIABLE_NUMBER,       "SQLITE_MAX_VARIABLE_NUMBER"      },
-  { SQLITE_LIMIT_TRIGGER_DEPTH,         "SQLITE_MAX_TRIGGER_DEPTH"        },
-  { SQLITE_LIMIT_WORKER_THREADS,        "SQLITE_MAX_WORKER_THREADS"       },
+typedef struct {
+    int limitCode;
+    const char *limitName;
+} LimitData;
+
+static const LimitData limitData[] = {
+    { SQLITE_LIMIT_LENGTH, "SQLITE_MAX_LENGTH" },
+    { SQLITE_LIMIT_SQL_LENGTH, "SQLITE_MAX_SQL_LENGTH" },
+    { SQLITE_LIMIT_COLUMN, "SQLITE_MAX_COLUMN" },
+    { SQLITE_LIMIT_EXPR_DEPTH, "SQLITE_MAX_EXPR_DEPTH" },
+    { SQLITE_LIMIT_COMPOUND_SELECT, "SQLITE_MAX_COMPOUND_SELECT" },
+    { SQLITE_LIMIT_VDBE_OP, "SQLITE_MAX_VDBE_OP" },
+    { SQLITE_LIMIT_FUNCTION_ARG, "SQLITE_MAX_FUNCTION_ARG" },
+    { SQLITE_LIMIT_ATTACHED, "SQLITE_MAX_ATTACHED" },
+    { SQLITE_LIMIT_LIKE_PATTERN_LENGTH, "SQLITE_MAX_LIKE_PATTERN_LENGTH" },
+    { SQLITE_LIMIT_VARIABLE_NUMBER, "SQLITE_MAX_VARIABLE_NUMBER" },
+    { SQLITE_LIMIT_TRIGGER_DEPTH, "SQLITE_MAX_TRIGGER_DEPTH" },
+    { SQLITE_LIMIT_WORKER_THREADS, "SQLITE_MAX_WORKER_THREADS" }
 };
 
-static int maxLimit(sqlite3 *db, int eCode){
-  int iOrig = sqlite3_limit(db, eCode, 0x7fffffff);
-  return sqlite3_limit(db, eCode, iOrig);
+static int getMaxLimit(sqlite3 *db, int limitCode) {
+    const int initialLimit = sqlite3_limit(db, limitCode, 0x7FFFFFFF);
+    return sqlite3_limit(db, limitCode, initialLimit);
 }
 
-int main(int argc, char **argv){
-  sqlite3 *db;
-  int j, rc;
-  rc = sqlite3_open(":memory:", &db);
-  if( rc==SQLITE_OK ){
-    for(j=0; j<sizeof(aLimit)/sizeof(aLimit[0]); j++){
-      printf("%-35s %10d\n", aLimit[j].zName, maxLimit(db, aLimit[j].eCode));
+static void printLimits(sqlite3 *db) {
+    const size_t count = sizeof(limitData) / sizeof(LimitData);
+
+    for (size_t i = 0; i < count; ++i) {
+        const LimitData *limit = &limitData[i];
+        printf("%-35s %10d\n", limit->limitName, getMaxLimit(db, limit->limitCode));
     }
-    sqlite3_close(db);
-  } 
+}
+
+int main(int argc, char **argv) {
+    sqlite3 *db;
+    const int result = sqlite3_open(":memory:", &db);
+
+    if (result == SQLITE_OK) {
+        printLimits(db);
+        sqlite3_close(db);
+    }
+
+    return 0;
 }
