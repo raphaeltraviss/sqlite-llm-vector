95d833c6a901924a14291e1cb20fa57184e40f48
Raphael Traviss <raphael@skyleafdesign.com>
2023-05-29 13:22:08 -0500

    feat: Add SQLite extension to implement noop() function for testing (80 chars)

--- ext/misc/noop.c
+++ ext/misc/noop.c
@@ -18,10 +18,24 @@ SQLITE_EXTENSION_INIT1
 #include <string.h>
 
 /* Implementation of the noop function. Returns the argument of this function */
+
+typedef struct NoopFunc NoopFunc;
+struct NoopFunc {
+    sqlite3_value *arg;
+};
+
 static void noopfunc(sqlite3_context *context, int argc, sqlite3_value **argv)
 {
     assert(argc == 1);
-    sqlite3_result_value(context, argv[0]);
+    NoopFunc *p = sqlite3_user_data(context);
+    if(p == NULL){
+        p = (NoopFunc*)sqlite3_malloc(sizeof(NoopFunc));
+        p->arg = argv[0];
+        sqlite3_result_value(context, p->arg);
+        sqlite3_user_data(context,p);
+    }else{
+        sqlite3_result_value(context, p->arg);
+    }
 }
 
 #ifdef _WIN32
@@ -35,25 +49,33 @@ int sqlite3_noop_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *
 
     (void)pzErrMsg;  /* Unused parameter */
 
+    NoopFunc *p = (NoopFunc*)sqlite3_malloc(sizeof(NoopFunc));
+
     rc = sqlite3_create_function(
-        db, "noop", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, noopfunc, 0, 0
+        db, "noop_nd", 1, SQLITE_UTF8, p, noopfunc, 0, 0
     );
     if( rc != SQLITE_OK ) return rc;
+
+    p = (NoopFunc*)sqlite3_malloc(sizeof(NoopFunc));
 
     rc = sqlite3_create_function(
         db, "noop_i", 1,
-        SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_INNOCUOUS, 0, noopfunc, 0, 0
+        SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_INNOCUOUS, p, noopfunc, 0, 0
     );
     if( rc != SQLITE_OK ) return rc;
+
+    p = (NoopFunc*)sqlite3_malloc(sizeof(NoopFunc));
 
     rc = sqlite3_create_function(
         db, "noop_do", 1,
-        SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_DIRECTONLY, 0, noopfunc, 0, 0
+        SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_DIRECTONLY, p, noopfunc, 0, 0
     );
     if( rc != SQLITE_OK ) return rc;
 
+    p = (NoopFunc*)sqlite3_malloc(sizeof(NoopFunc));
+
     rc = sqlite3_create_function(
-        db, "noop_nd", 1, SQLITE_UTF8, 0, noopfunc, 0, 0
+        db, "noop", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, p, noopfunc, 0, 0
     );
     return rc;
 } 
