684b81374c017d9d8972d2d2558dded419a71603
Raphael Traviss <raphael@skyleafdesign.com>
2023-05-30 08:55:47 -0500

    Add error handling and use efficient SQLite functions in main() function.

--- ext/wasm/example_extra_init.c
+++ ext/wasm/example_extra_init.c
@@ -1,5 +1,6 @@
 #include "sqlite3.h"
 #include <stdio.h>
+#include <stdlib.h> // Added header for exit() function
 
 //error handling function
 static inline void handle_errors (const char * message, int result) {
@@ -16,12 +17,14 @@ int sqlite3_wasm_extra_init(const char *z){
 
 int main() {
     //initialize database connection
-    sqlite3 *db;
-    int result = sqlite3_open("test.db", &db);
+    sqlite3 *db = NULL; // Set the database pointer to null
+    char *sql = NULL; // Set the SQL string pointer to null
+    int result = sqlite3_open_v2("test.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
     handle_errors("Failed to initialize database connection.", result);
+    sqlite3_busy_timeout(db, 5000); // Set a busy timeout to avoid busy operations
 
     //create table
-    char *sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);";
+    sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);";
     result = sqlite3_exec(db, sql, NULL, NULL, NULL);
     handle_errors("Failed to create table.", result);
 
@@ -35,6 +38,12 @@ int main() {
     result = sqlite3_exec(db, sql, NULL, NULL, NULL);
     handle_errors("Failed to query data.", result);
 
-    sqlite3_close(db);
+    // Free the sql pointer
+    sqlite3_free(sql);
+    sql = NULL;
+
+    sqlite3_close_v2(db); // Use the sqlite3_close_v2 function to release resources efficiently
+    db = NULL;
+
     return EXIT_SUCCESS;
 }
