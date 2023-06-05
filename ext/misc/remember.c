6e276d4c929886bab7053f92f9a89ce6359e0f5b
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-05 10:33:56 -0500

    feat: add "remember" function to SQLite3 extension

--- ext/misc/remember.c
+++ ext/misc/remember.c
@@ -1,17 +1,4 @@
-/*
-** 2016-08-09
-**
-** The author disclaims copyright to this source code.  In place of
-** a legal notice, here is a blessing:
-**
-**    May you do good and not evil.
-**    May you find forgiveness for yourself and forgive others.
-**    May you share freely, never taking more than you give.
-*/
-
-#include "sqlite3ext.h"
-SQLITE_EXTENSION_INIT1
-
+#include <sqlite3.h>
 #include <assert.h>
 
 /**
@@ -20,30 +7,31 @@ SQLITE_EXTENSION_INIT1
  * Return the integer value V. Also, save the value of V in a
  * C-language variable whose address is PTR.
  */
-static void rememberFunc(sqlite3_context *ctx, int argc, sqlite3_value **argv)
+static void remember_func(sqlite3_context *ctx, int argc, sqlite3_value **argv)
 {
-    sqlite3_int64 v;
     sqlite3_int64 *ptr;
+    sqlite3_int64 v;
+
     assert(argc == 2);
 
     v = sqlite3_value_int64(argv[0]);
     ptr = sqlite3_value_pointer(argv[1], "carray");
 
-    if (ptr) {
+    if (ptr)
+    {
         *ptr = v;
     }
 
     sqlite3_result_int64(ctx, v);
 }
 
-#ifdef _WIN32
-__declspec(dllexport)
-#endif
 int sqlite3_remember_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi)
 {
+    int rc = SQLITE_ERROR;
+
     SQLITE_EXTENSION_INIT2(pApi);
 
-    int rc = sqlite3_create_function(db, "remember", 2, SQLITE_UTF8, 0, rememberFunc, 0, 0);
+    rc = sqlite3_create_function(db, "remember", 2, SQLITE_UTF8, 0, remember_func, 0, 0);
 
     return rc;
 }
