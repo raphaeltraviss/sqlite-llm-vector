74c2b46b78b855a107af83de0b34c6ffef7c2436
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-05 10:35:08 -0500

    feat: Add custom collation function using SQLite extension API

--- ext/misc/anycollseq.c
+++ ext/misc/anycollseq.c
@@ -1,20 +1,29 @@
-#define CMP(p1, p2, n) (rc = memcmp(p1, p2, n)) == 0 ? nKey1 - nKey2 : rc
+#include <string.h>
+#include <sqlite3ext.h>
+SQLITE_EXTENSION_INIT1
 
-static int anyCollFunc(void *NotUsed, int nKey1, const void *pKey1, int nKey2, const void *pKey2) {
-    int rc, n = nKey1 < nKey2 ? nKey1 : nKey2;
-    return CMP(pKey1, pKey2, n);
+static int cmp(void *_, int n1, const void *p1, int n2, const void *p2) {
+    int rc;
+    const int n = (n1 < n2 ? n1 : n2);
+    for (int i = 0; i < n; ++i) {
+        if (((const char *)p1)[i] != ((const char *)p2)[i]) {
+            rc = ((const char *)p1)[i] - ((const char *)p2)[i];
+            return (rc > 0 ? 1 : (rc < 0 ? -1 : n1 - n2));
+        }
+    }
+   return (n1 - n2); 
 }
 
-static void anyCollNeeded(void *NotUsed, sqlite3 *db, int eTextRep, const char *zCollName) {
-    sqlite3_create_collation(db, zCollName, eTextRep, 0, anyCollFunc); 
+static void create(sqlite3_api_routines *pRoutines, sqlite3 *db,
+                   void *NotUsed, int textRep, const char *coll) {
+    (void)NotUsed;
+    pRoutines->sqlite3_create_collation(db, coll, textRep, NULL, cmp);
 }
 
-#ifdef _WIN32
-__declspec(dllexport)
-#endif
-int sqlite3_anycollseq_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
-    int rc = SQLITE_OK;
-    SQLITE_EXTENSION_INIT2(pApi);
-    rc = sqlite3_collation_needed(db, 0, anyCollNeeded);
-    return rc;
+int sqlite3_anycollseq_init(sqlite3 *db, char **pzErrMsg, 
+                            const sqlite3_api_routines *pRoutines) {
+    (void)pzErrMsg;
+    SQLITE_EXTENSION_INIT2(pRoutines);
+    pRoutines->sqlite3_collation_needed(db, NULL, create);
+    return SQLITE_OK;
 }
