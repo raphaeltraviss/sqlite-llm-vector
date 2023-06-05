7f83b48bf96cfe7d80197fc79db5c9fae41e526f
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-05 11:19:51 -0500

    feat: Add program to read specified number of bytes from a file at specified offset

--- tool/extract.c
+++ tool/extract.c
@@ -1,46 +1,70 @@
-/*
-** Extract a range of bytes from a file.
-**
-** Usage:
-**
-**    extract FILENAME OFFSET AMOUNT
-**
-** The bytes are written to standard output.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv){
-  FILE *f;
-  char *zBuf;
-  int ofst;
-  int n;
-  size_t got;
+/* Function to display usage information */
+void display_usage(char *program_name){
+    fprintf(stderr, "Usage: %s FILENAME OFFSET AMOUNT\n", program_name);
+}
 
-  if( argc!=4 ){
-    fprintf(stderr, "Usage: %s FILENAME OFFSET AMOUNT\n", *argv);
-    return 1;
-  }
-  f = fopen(argv[1], "rb");
-  if( f==0 ){
-    fprintf(stderr, "cannot open \"%s\"\n", argv[1]);
-    return 1;
-  }
-  ofst = atoi(argv[2]);
-  n = atoi(argv[3]);
-  zBuf = malloc( n );
-  if( zBuf==0 ){
-    fprintf(stderr, "out of memory\n");
-    return 1;
-  }
-  fseek(f, ofst, SEEK_SET);
-  got = fread(zBuf, 1, n, f);
-  fclose(f);
-  if( got<n ){
-    fprintf(stderr, "got only %d of %d bytes\n", got, n);
-    return 1;
-  }else{
-    fwrite(zBuf, 1, n, stdout);
-  }
-  return 0;
+/* Function to open file */
+FILE *open_file(char *filename){
+    FILE *file = fopen(filename, "rb");
+    if(file == NULL){
+        fprintf(stderr, "cannot open \"%s\"\n", filename);
+    }
+    return file;
+}
+
+/* Function to read bytes from file */
+size_t read_from_file(FILE *f, int offset, char *buffer, int n){
+    fseek(f, offset, SEEK_SET);
+    return fread(buffer, 1, n, f);
+}
+
+/* Function to write bytes to output */
+void write_to_output(char *buffer, int n){
+    fwrite(buffer, 1, n, stdout);
+}
+
+int main(int argc, char **argv){
+    FILE *f;
+    char *zBuf;
+    int ofst, n;
+    size_t got;
+
+    if(argc!=4){
+        display_usage(*argv);
+        return 1;
+    }
+
+    f = open_file(argv[1]);
+    if(f == NULL){
+        return 1;
+    }
+
+    ofst = atoi(argv[2]);
+    n = atoi(argv[3]);
+    if(n <= 0){
+        fprintf(stderr, "invalid amount of bytes\n");
+        return 1;
+    }
+
+    zBuf = malloc(n);
+    if(zBuf == NULL){
+        fprintf(stderr, "out of memory\n");
+        return 1;
+    }
+
+    got = read_from_file(f, ofst, zBuf, n);
+    fclose(f);
+
+    if(got < n){
+        fprintf(stderr, "got only %d of %d bytes\n", (int)got, n);
+        return 1;
+    }else{
+        write_to_output(zBuf, n);
+    }
+
+    free(zBuf);
+    return 0;
 }
