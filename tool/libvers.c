c972c5a0fd36b78731089026d427f3e9467f8c48
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-07 09:46:17 -0500

    feat: Add SQLite initialization, shutdown, and version print functions.

--- tool/libvers.c
+++ tool/libvers.c
@@ -2,20 +2,33 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
-// Declare global pointers
-const char *version = NULL, *source = NULL;
+#define MAX_LIBRARY_VERSION_LENGTH 256
+#define MAX_SOURCE_ID_LENGTH 256
 
-int init_sqlite() {
+static int init_sqlite(sqlite3 **db) {
     int rc = sqlite3_initialize();
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Failed to initialize SQLite library: %s\n", sqlite3_errstr(rc)); 
         return rc;
     }
+
+    rc = sqlite3_open(":memory:", db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to create database connection: %s\n", sqlite3_errmsg(*db));
+        return rc;
+    }
+    
     return SQLITE_OK;
 }
 
-int shutdown_sqlite() {
-    int rc = sqlite3_shutdown();
+static int shutdown_sqlite(sqlite3 *db) {
+    int rc = sqlite3_close(db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to close SQLite library: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+
+    rc = sqlite3_shutdown();
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Failed to shutdown SQLite library: %s\n", sqlite3_errstr(rc)); 
         return rc;
@@ -23,48 +36,41 @@ int shutdown_sqlite() {
     return SQLITE_OK;
 }
 
-int get_version() {
-    version = sqlite3_libversion();
-    if (version == NULL) {
+static void print_library_version_and_source_id() {
+    char version[MAX_LIBRARY_VERSION_LENGTH], source[MAX_SOURCE_ID_LENGTH];
+
+    const char *lib_version = sqlite3_libversion();
+    if (lib_version == NULL) {
         fprintf(stderr, "Failed to retrieve SQLite library version\n");
-        return SQLITE_ERROR;
+        return;
     }
-    return SQLITE_OK;
-}
 
-int get_source() {
-    source = sqlite3_sourceid();
-    if (source == NULL) {
+    const char *source_id = sqlite3_sourceid();
+    if (source_id == NULL) {
         fprintf(stderr, "Failed to retrieve SQLite library source ID\n");
-        return SQLITE_ERROR;
-    }
-    return SQLITE_OK;
-}
-
-int main(int argc, char **argv){
-
-    int rc = EXIT_SUCCESS;
-
-    rc = init_sqlite();
-    if (rc != SQLITE_OK) {
-        goto cleanup;
+        return;
     }
 
-    rc = get_version();
-    if (rc != SQLITE_OK) {
-        goto cleanup;
-    }
-
-    rc = get_source();
-    if (rc != SQLITE_OK) {
-        goto cleanup;
-    }
+    snprintf(version, MAX_LIBRARY_VERSION_LENGTH, "%s", lib_version);
+    snprintf(source, MAX_SOURCE_ID_LENGTH, "%s", source_id);
 
     printf("SQLite version %s\n", version);
     printf("SQLite source  %s\n", source);
+}
+
+int main(int argc, char **argv) {
+    int rc = EXIT_SUCCESS;
+    sqlite3 *db = NULL;
+
+    rc = init_sqlite(&db);
+    if (rc != SQLITE_OK) {
+        goto cleanup;
+    }
+
+    print_library_version_and_source_id();
 
 cleanup:
-    rc = shutdown_sqlite();
+    rc = shutdown_sqlite(db);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Failed to shutdown SQLite library: %s\n", sqlite3_errstr(rc)); 
         return EXIT_FAILURE;
