d44cbafa0179b299b5ddbe957aea6b9db1a6f660
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-09 17:39:26 -0500

    Create 'remember' and 'clientData' functions in SQLite extension.

--- ext/misc/remember.c
+++ ext/misc/remember.c
@@ -17,7 +17,7 @@ static void remember_func(sqlite3_context *ctx, int argc, sqlite3_value **argv) 
         sqlite3_result_error(ctx, "Failed to retrieve pointer value", -1);
         return;
     }
-    ClientData *data = sqlite3_user_data(ctx);
+    ClientData *data = (ClientData*)sqlite3_user_data(ctx);
     sqlite3_int64 v = sqlite3_value_int64(argv[0]);
     *ptr = v;
     data->value = v;
@@ -26,18 +26,23 @@ static void remember_func(sqlite3_context *ctx, int argc, sqlite3_value **argv) 
 
 static int sqlite3_remember_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
     int rc = SQLITE_ERROR;
-    sqlite3_create_function_v2(db, "remember", 2, SQLITE_UTF8, NULL, &remember_func, NULL, NULL, &sqlite3_free);
-    return SQLITE_OK;
+    rc = sqlite3_create_function_v2(db, "remember", 2, SQLITE_UTF8, NULL, &remember_func, NULL, NULL, &free);
+    return rc;
 }
 
 int sqlite3_extension_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
     int rc = SQLITE_ERROR;
-
+    ClientData* clientData = (ClientData*)malloc(sizeof(ClientData));
+    if (!clientData) {
+        return SQLITE_ERROR;
+    }
+    clientData->value = 0;
     rc = sqlite3_remember_init(db, pzErrMsg, pApi);
     if (rc != SQLITE_OK) {
         sqlite3_free(*pzErrMsg);
+        free(clientData);
         return rc;
     }
-
+    sqlite3_create_function_v2(db, "clientData",0, SQLITE_UTF8, clientData,&clientDataFunc, NULL,NULL, &free);
     return rc;
 }
