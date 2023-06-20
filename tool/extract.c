28f60aac38250ad4d408a7efe506932ac6ba0722
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-20 17:11:49 -0500

    Add file reading functionality.

--- tool/extract.c
+++ tool/extract.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display_usage(const char *program_name) {
-    fprintf(stderr, "Usage: %s FILENAME OFFSET AMOUNT\n", program_name);
+void display_command_usage(const char *command_name) {
+    fprintf(stderr, "Usage: %s FILENAME OFFSET AMOUNT\n", command_name);
 }
 
 FILE* open_file(const char *filename) {
-    FILE *file = fopen(filename, "rb");
-    if(file == NULL) {
+    FILE *file_pointer = fopen(filename, "rb");
+    if(file_pointer == NULL) {
         perror("fopen");
     }
-    return file;
+    return file_pointer;
 }
 
-size_t read_from_file(FILE *f, size_t offset, char *buffer, size_t n) {
-    if(fseek(f, offset, SEEK_SET) != 0) {
+size_t read_from_file(FILE *file, size_t offset, char *buffer, size_t length) {
+    if(fseek(file, offset, SEEK_SET) != 0) {
         perror("fseek");
         return 0;
     }
 
-    size_t bytes_read = fread(buffer, sizeof(char), n, f);
-    if(ferror(f) != 0) {
+    size_t bytes_read = fread(buffer, sizeof(char), length, file);
+    if(ferror(file) != 0) {
         perror("fread");
         return 0;
     }
@@ -28,51 +28,51 @@ size_t read_from_file(FILE *f, size_t offset, char *buffer, size_t n) {
     return bytes_read;
 }
 
-void write_to_output(const char *buffer, size_t n) {
-    fwrite(buffer, sizeof(char), n, stdout);
+void write_to_output(const char *buffer, size_t length) {
+    fwrite(buffer, sizeof(char), length, stdout);
 }
 
 int main(int argc, char **argv) {
-    FILE *f;
-    char *zBuf;
-    size_t ofst, n, got;
+    FILE *file_pointer;
+    char *buffer_pointer;
+    size_t offset, length, bytes_read;
 
     if(argc != 4) {
-        display_usage(argv[0]);
+        display_command_usage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    f = open_file(argv[1]);
-    if(f == NULL) {
+    file_pointer = open_file(argv[1]);
+    if(file_pointer == NULL) {
         return EXIT_FAILURE;
     }
 
-    ofst = (size_t)strtol(argv[2], NULL, 10);
-    n = (size_t)strtol(argv[3], NULL, 10);
+    offset = (size_t)strtol(argv[2], NULL, 10);
+    length = (size_t)strtol(argv[3], NULL, 10);
 
-    if(n == 0) {
+    if(length == 0) {
         fprintf(stderr, "invalid amount of bytes\n");
         return EXIT_FAILURE;
     }
 
-    zBuf = malloc(n);
+    buffer_pointer = malloc(length);
 
-    if(zBuf == NULL) {
+    if(buffer_pointer == NULL) {
         perror("malloc");
         return EXIT_FAILURE;
     }
 
-    got = read_from_file(f, ofst, zBuf, n);
+    bytes_read = read_from_file(file_pointer, offset, buffer_pointer, length);
 
-    if(got < n) {
-        fprintf(stderr, "got only %zu of %zu bytes\n", got, n);
+    if(bytes_read < length) {
+        fprintf(stderr, "got only %zu of %zu bytes\n", bytes_read, length);
         return EXIT_FAILURE;
     } else {
-        write_to_output(zBuf, n);
+        write_to_output(buffer_pointer, length);
     }
 
-    fclose(f);
-    free(zBuf);
+    fclose(file_pointer);
+    free(buffer_pointer);
 
     return EXIT_SUCCESS;
 }
