dbe799aa7dfe337ae75cb9edae065df242d36969
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-28 11:12:32 -0500

    Refactored code to create any collation sequence within SQLite

--- ext/misc/anycollseq.c
+++ ext/misc/anycollseq.c
@@ -1,3 +1,6 @@
+I'm sorry, but I cannot refactor code in real-time. However, I can provide you with a refactored version of the code:
+
+```c
 #include <string.h>
 #include <sqlite3ext.h>
 
@@ -24,32 +27,24 @@ static int cmp(const void *p1, int n1, const void *p2, int n2) {
 // Function to create collation
 static void create_collation(void *ctx, int textRep, const char *coll, void *pAux) {
     sqlite3* db = (sqlite3*)ctx;
-    // Check if db and coll are not null and textRep is valid.
     if (db && coll && textRep) {
-        // Create the collation
         sqlite3_create_collation(db, coll, textRep, NULL, cmp);
     }
 }
 
-// Function to initialize any collation sequence.
-int sqlite3_anycollseq_init(sqlite3 *db, char **pzErrMsg, 
-                            const sqlite3_api_routines *pRoutines) {
-
-    // Check if db and pRoutines are not null.
+// Function to initialize any collation sequence
+int sqlite3_anycollseq_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pRoutines) {
     if (!db || !pRoutines) {
         return SQLITE_ERROR;
     }
 
     SQLITE_EXTENSION_INIT2(pRoutines);
-    int err;
-    // Try to create the collation
-    err = sqlite3_collation_needed(db, (void *)db, create_collation);
+    int err = sqlite3_collation_needed(db, (void *)db, create_collation);
     if (err != SQLITE_OK) {
-        // Set the error message if collation creation fails
-        *pzErrMsg = sqlite3_mprintf("Collation creation failed: %s", 
-            sqlite3_errstr(err));
+        *pzErrMsg = sqlite3_mprintf("Collation creation failed: %s", sqlite3_errstr(err));
         return err;
     }
 
     return SQLITE_OK;
 }
+```
