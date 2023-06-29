98eb4ddbf457c256a85eb61226b71657059682a3
Raphael Traviss <raphael@skyleafdesign.com>
2023-06-29 09:14:38 -0500

    Read and display a portion of a file.

--- tool/extract.c
+++ tool/extract.c
@@ -1,36 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display_command_usage(const char *command_name) {
-    fprintf(stderr, "Usage: %s FILENAME OFFSET AMOUNT\n", command_name);
-}
-
-FILE* open_file(const char *filename) {
-    FILE *file_pointer = fopen(filename, "rb");
-    if(file_pointer == NULL) {
-        perror("fopen");
-    }
-    return file_pointer;
-}
-
-size_t read_from_file(FILE *file, size_t offset, char *buffer, size_t length) {
-    if(fseek(file, offset, SEEK_SET) != 0) {
-        perror("fseek");
-        return 0;
-    }
-
-    size_t bytes_read = fread(buffer, sizeof(char), length, file);
-    if(ferror(file) != 0) {
-        perror("fread");
-        return 0;
-    }
-
-    return bytes_read;
-}
-
-void write_to_output(const char *buffer, size_t length) {
-    fwrite(buffer, sizeof(char), length, stdout);
-}
+#define display_command_usage(command_name) fprintf(stderr, "Usage: %s FILENAME OFFSET AMOUNT\n", command_name)
+#define OPEN_FILE(filename) fopen(filename, "rb")
+#define READ_FROM_FILE(file, offset, buffer, length) fread(buffer, sizeof(char), length, file)
+#define WRITE_TO_OUTPUT(buffer, length) fwrite(buffer, sizeof(char), length, stdout)
 
 int main(int argc, char **argv) {
     FILE *file_pointer;
@@ -42,8 +16,9 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    file_pointer = open_file(argv[1]);
+    file_pointer = OPEN_FILE(argv[1]);
     if(file_pointer == NULL) {
+        perror("fopen");
         return EXIT_FAILURE;
     }
 
@@ -56,19 +31,27 @@ int main(int argc, char **argv) {
     }
 
     buffer_pointer = malloc(length);
-
     if(buffer_pointer == NULL) {
         perror("malloc");
         return EXIT_FAILURE;
     }
 
-    bytes_read = read_from_file(file_pointer, offset, buffer_pointer, length);
+    if(fseek(file_pointer, offset, SEEK_SET) != 0) {
+        perror("fseek");
+        return EXIT_FAILURE;
+    }
+
+    bytes_read = READ_FROM_FILE(file_pointer, offset, buffer_pointer, length);
+    if(ferror(file_pointer) != 0) {
+        perror("fread");
+        return EXIT_FAILURE;
+    }
 
     if(bytes_read < length) {
         fprintf(stderr, "got only %zu of %zu bytes\n", bytes_read, length);
         return EXIT_FAILURE;
     } else {
-        write_to_output(buffer_pointer, length);
+        WRITE_TO_OUTPUT(buffer_pointer, length);
     }
 
     fclose(file_pointer);
